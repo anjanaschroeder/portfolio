11d6e25541f101eb24d068ed2e9e7b9843564ca7
anjanaschroeder <31491612+anjanaschroeder@users.noreply.github.com>
2017-08-31 17:38:21 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+function myFunction() {
+    var topNavigation
+    var homeButton = document.getElementById("home-button");
+    var aboutButton = document.getElementById("about-button");
+    var contactButton = document.getElementById("contact-button");
+    var listItems = document.getElementById("our-list").getElementsByTagName("li");
+    if (x.topNavigation === "topnav") {
+        x.topNavigation += " responsive";
+    } else {
+        x.topNavigation = "topnav";
+    }
+}
+
+function thankYou(string) {
+	string = string.toSubmit()
+	return = sayThankYou(string)
+}
+ 
+console.log(thankYou("thank you for your note"))
+
+<!--code I'll need to understand
+
+<script type="text/javascript">
+<!--Hide this from browswers that don't understand Javascript
+
+function editNodeText(id,newText){
+
+var node = document.getElementById(id);
+while (node.firstChild)
+node.removeChild(node.firstChild);
+node.appendChild(document.createTextNode(newText));
+}
+
+function editNoteText(ifItem1, ifItem2)
+{
+
+var valuePassed1=ifItem1;
+var valuePassed2=ifItem2;
+var valueToReturn = "";
+var node;
+
+  <span id="ifSpanEx" onclick="changeValue(this)">Thank you for your note!</span>
+  <button id="Send" onclick="changeValue(this)">Thank you for your note!</button>
+
+switch (valuePassed) {
+
+default: valueToReturn2="Thank you for your note!";
+}
+
+<bondy onresize="resizeTheLogo();">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
